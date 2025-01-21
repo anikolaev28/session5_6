340f819d3cb225fd0d8fbbbb854781c206481583
alejandro <anikolaev.ieu2023@student.ie.edu>
2025-01-21 17:39:55 +0100

    Second commit

--- Exceptions_examples.py
+++ Exceptions_examples.py
@@ -4,13 +4,14 @@ age = input("How old are you? ",)
 try:
     # another way to format print is via f-strings
     print(f"{name}, you were born in {2024-int(age)}")
-    division = int(age) / 0
 except ValueError:
     print("Age must be a valid number")
     print(f"The value that you typed was {age}")
 except ZeroDivisionError:
-    print("You can't divide by 0")
-except:
+    print("You can't divide by zero")
+except: # all the other type of exceptions
     print("No idea what else can go wrong, but just in case.")
+else: # in case no exception happened
+    print("Thanks for being a good sport and not trying to crash the app")
 print(f"{name}, thanks for playing")
 
